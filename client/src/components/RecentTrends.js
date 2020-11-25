c8db7d86747bc76ec7e99c4755a971ea59f72d06
rosie-123 <rcao34@wisc.edu>
2020-11-25 02:04:39 -0600

    load initial word cloud

--- client/src/components/RecentTrends.js
+++ client/src/components/RecentTrends.js
@@ -9,33 +9,48 @@ import {
   CardContent,
   CardHeader,
 } from "@material-ui/core";
+import WordCloud from "react-d3-cloud";
 import { makeStyles } from "@material-ui/core/styles";
 
 const RecentTrends = () => {
   const today = new Date();
-  const initStr = moment(new Date()).subtract(1, "days").format("YYYY-MM-DD")
+  const initStr = moment(new Date()).subtract(1, "days").format("YYYY-MM-DD");
   const [sliderVal, setSliderVal] = useState(15);
   const [termsDict, setTermsDict] = useState({});
-  const [dateDict, setDateDict] = useState({});
+  const [dateDict, setDateDict] = useState(undefined);
+  
   useEffect(() => {
     fetch("http://localhost:9000/trends")
       .then((res) => res.json())
       .then((res) => {
         setTermsDict(res);
         for (const date of Object.keys(res)) {
-          if (moment(date).format('YYYY-MM-DD') === initStr) {
-            setDateDict(res[date])
+          if (moment(date).format("YYYY-MM-DD") === initStr) {
+            const cloudData = [];
+            Object.keys(res[date]).forEach((key) => {
+              const formattedDict = {};
+              let searchVolumn = res[date][key].slice(0, -1);
+              searchVolumn =
+                searchVolumn.slice(-1) === "M"
+                  ? searchVolumn.slice(0, -1) + "0"
+                  : searchVolumn.slice(0, -1);
+              formattedDict["text"] = key;
+              formattedDict["value"] = Number(searchVolumn);
+              cloudData.push(formattedDict);
+            });
+            setDateDict(cloudData);
           }
         }
       });
   }, []);
   const handleSlide = (event, newValue) => {
     setSliderVal(newValue);
+    // TODO set sliding animation
   };
+  const fontSizeMapper = (word) => Math.log2(word.value) * 5;
   const classes = useStyles();
   return (
     <React.Fragment>
-      {console.log(dateDict)}
       <Card className={classes.root}>
         <CardHeader
           title="Recent Trending"
@@ -56,6 +71,13 @@ const RecentTrends = () => {
           <Moment format="YYYY-MM-DD" subtract={{ day: 15 - sliderVal }}>
             {initStr}
           </Moment>
+          {dateDict ? (
+            <WordCloud width={450} height={350} data={dateDict} fontSizeMapper={fontSizeMapper} />
+          ) : (
+            <React.Fragment>
+              <Typography>The content is loading ...</Typography>
+            </React.Fragment>
+          )}
         </CardContent>
       </Card>
     </React.Fragment>
